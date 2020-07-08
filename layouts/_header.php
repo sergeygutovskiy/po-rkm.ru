67c1bdaac30a079313130ebffe388a52dac1a88e
sergeygutovskiy <sergeygutovsk@gmail.com>
2020-07-08 18:18:03 +0300

    navigation for tabltes

--- layouts/_header.php
+++ layouts/_header.php
@@ -7,3 +7,5 @@ rel="stylesheet">
 <link rel="stylesheet" type="text/css" href="css/_navigation.css">
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
--- layouts/_navigation.php
+++ layouts/_navigation.php
@@ -16,9 +16,17 @@
 			<img src="img/navigation-logo.png" alt="логотип" class="navigation__image">
 		</div>
 
+		<div class="navigation__hamburger">
+			<svg viewBox="0 0 800 600">
+				<path d="M300,220 C300,220 520,220 540,220 C740,220 640,540 520,420 C440,340 300,200 300,200" class="top"></path>
+				<path d="M300,320 L540,320" class="middle"></path>
+				<path d="M300,210 C300,210 520,210 540,210 C740,210 640,530 520,410 C440,330 300,190 300,190" class="bottom" transform="translate(480, 320) scale(1, -1) translate(-480, -318) "></path>
+			</svg>
+		</div>  
+
 		<ul class="navigation__list">
 			<li class="navigation__item">
-				<a href="" class="navigation__link">
+				<a href="javascript:void(0);" class="navigation__link">
 					Каталог
 					<i class="far fa-angle-down navigation__arrow-down"></i>
 				</a>
@@ -42,7 +50,7 @@
 			</li>
 
 			<li class="navigation__item">
-				<a href="" class="navigation__link">
+				<a href="javascript:void(0);" class="navigation__link">
 					Услуги
 					<i class="far fa-angle-down navigation__arrow-down"></i>
 				</a>
@@ -70,7 +78,7 @@
 			</li>
 
 			<li class="navigation__item">
-				<a href="" class="navigation__link">
+				<a href="javascript:void(0);" class="navigation__link">
 					О компании
 					<i class="far fa-angle-down navigation__arrow-down"></i>
 				</a>
@@ -93,3 +101,13 @@
 		<button class="navigation__button">Оставить заявку</button>
 	</div>
 </nav>
+
+<script type="text/javascript">
+	
+	$('.navigation__hamburger').on('click', function(e) {
+		e.preventDefault();
+		$('.navigation__hamburger').toggleClass("navigation__hamburger--active");
+	    $(".navigation__list").toggleClass("navigation__list--active")
+	});
+
+</script>
