4d1646c30e0712ceca41cb5c36a9cfc84f01002b
sergeygutovskiy <sergeygutovsk@gmail.com>
2020-07-10 20:31:01 +0300

    mnogo huyni

--- index.php
+++ index.php
@@ -2,22 +2,34 @@
 <html>
 <head>
 	<title>Index</title>
-
 	<?php require_once "layouts/_header.php"; ?>
+
+	<link rel="stylesheet" type="text/css" href="css/index.css">
 
 </head>
 <body>
 
 	<?php require_once "layouts/_navigation.php"; ?>
 
-	<p>
-		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-	</p>
+	<div class="ndx-header main-container">
+		<div class="ndx-header__content">
+			<img src="img/index-logo.png" class="ndx-header__logo">
+			<ul class="ndx-header__list">
+				<li class="ndx-header__item">
+					<div></div>Художественная ковка
+				</li>
+				<li class="ndx-header__item">
+					<div></div>Реставрация исторических решеток
+				</li>
+				<li class="ndx-header__item">
+					<div></div>Производство металлоконструкций: ворот, заборов, ограждений
+				</li>	
+				<li class="ndx-header__item">
+					<div></div>Металлический декор: <br> валюты, кольца, вензеля
+				</li>
+			</ul>
+		</div>
+	</div>
 
 	<?php require_once "layouts/_footer.php"; ?>
 
