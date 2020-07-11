2f9cc195b4b288361cb6eb7df2198657ba0405e5
Kirill228sexy <kirillteterehov@yandex.ru->
2020-07-12 00:26:34 +0300

    xiuna

---
+++ gate.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ворота</title>
+	<?php require_once "layouts/_header.php"; ?>
+
+	<link rel="stylesheet" type="text/css" href="css/gate.css">
+
+</head>
+<body>
+
+	<?php require_once "layouts/_navigation.php"; ?>
+
+	<div class="ndx-header main-container">
+		<div class="ndx-header__content">
+			<div class="ndx-header__leftnav">
+				<ul>
+  					<a href="gate.php"><li>Ворота</li></a>
+					<a href="#"><li>Ограждения</li></a>
+					<a href="#"><li>Козырьки</li></a>
+					<a href="#"><li>Лестницы</li></a>
+					<a href="#"><li>Декор</li></a>
+					<a href="#"><li>Интерьерный декор</li></a>
+					
+				</ul>
+			</div>
+
+			
+
+		</div>
+		<div class="ndx-header__right-block">
+		<div class="ndx-header__mainbaner">
+		</div>
+		<div class="ndx-header__goodsblock">
+		
+				
+			
+			<div class="ndx-header__goodscard">
+				<div class="ndx-header__mainphoto">
+				<div class="ndx-header__readmore">
+						<p>Подробнее</p>
+					</div>
+					<img src="img\image 42.png">
+					
+				</div>
+				<div class="ndx-header__textaboutgoods">
+					<div class="ndx-header__namegoods">
+						<h3>Название название</h3>
+					</div>
+					<div class="ndx-header__textabout">
+						<p>Название</p>
+					</div>
+					<div class="ndx-header__price">
+						<p>От 1500 ₽</p>
+					</div>
+
+				</div>
+			</div>
+
+			<div class="ndx-header__goodscard">
+				<div class="ndx-header__mainphoto">
+				<div class="ndx-header__readmore">
+						<p>Подробнее</p>
+					</div>
+					<img src="img\index-header.png">
+					
+				</div>
+				<div class="ndx-header__textaboutgoods">
+					<div class="ndx-header__namegoods">
+						<h3>Название название</h3>
+					</div>
+					<div class="ndx-header__textabout">
+						<p>Название</p>
+					</div>
+					<div class="ndx-header__price">
+						<p>От 1500 ₽</p>
+					</div>
+
+				</div>
+			</div>
+
+			<div class="ndx-header__goodscard">
+				<div class="ndx-header__mainphoto">
+				<div class="ndx-header__readmore">
+						<p>Подробнее</p>
+					</div>
+					<img src="img\image 42.png">
+					
+				</div>
+				<div class="ndx-header__textaboutgoods">
+					<div class="ndx-header__namegoods">
+						<h3>Название название</h3>
+					</div>
+					<div class="ndx-header__textabout">
+						<p>Название</p>
+					</div>
+					<div class="ndx-header__price">
+						<p>От 1500 ₽</p>
+					</div>
+
+				</div>
+			</div>
+
+			<div class="ndx-header__goodscard">
+				<div class="ndx-header__mainphoto">
+				<div class="ndx-header__readmore">
+						<p>Подробнее</p>
+					</div>
+					<img src="img\image 42.png">
+					
+				</div>
+				<div class="ndx-header__textaboutgoods">
+					<div class="ndx-header__namegoods">
+						<h3>Название название</h3>
+					</div>
+					<div class="ndx-header__textabout">
+						<p>Название</p>
+					</div>
+					<div class="ndx-header__price">
+						<p>От 1500 ₽</p>
+					</div>
+
+				</div>
+			</div>
+
+			</div>
+		
+		</div>
+	</div>
+
+	<?php require_once "layouts/_footer.php"; ?>
+
+</body>
+</html>
