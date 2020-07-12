82588a708d527b743f06f2659f29a00e3f42b800
sergeygutovskiy <sergeygutovsk@gmail.com>
2020-07-13 01:15:59 +0300

    gallery block on index page

--- index.php
+++ index.php
@@ -4,6 +4,12 @@
 	<title>Index</title>
 	<?php require_once "layouts/_header.php"; ?>
 
+	<link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.css">
+	<link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
+
+	<script src="https://unpkg.com/swiper/swiper-bundle.js"></script>
+	<script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+
 	<link rel="stylesheet" type="text/css" href="css/index.css">
 
 </head>
@@ -133,6 +139,70 @@
 			</li>
 		</ul>
 	</div>
+
+	<div class="main-container">
+		<div class="main-title">Галерея</div>
+
+		<div class="gallery swiper-container">
+		    <!-- Additional required wrapper -->
+		    <div class="swiper-wrapper">
+		        <!-- Slides -->
+				<div class="swiper-slide swiper-slide gallery__slide">
+					<img src="img/index-gallery/1.webp" class="gallery__image">
+				</div>
+				<div class="swiper-slide swiper-slide gallery__slide">
+					<img src="img/index-gallery/2.webp" class="gallery__image">
+				</div>
+				<div class="swiper-slide swiper-slide gallery__slide">
+					<img src="img/index-gallery/3.webp" class="gallery__image">
+				</div>
+				<div class="swiper-slide swiper-slide gallery__slide">
+					<img src="img/index-gallery/4.webp" class="gallery__image">
+				</div>
+				<div class="swiper-slide swiper-slide gallery__slide">
+					<img src="img/index-gallery/5.webp" class="gallery__image">
+				</div>
+				<div class="swiper-slide swiper-slide gallery__slide">
+					<img src="img/index-gallery/6.webp" class="gallery__image">
+				</div>
+				<div class="swiper-slide swiper-slide gallery__slide">
+					<img src="img/index-gallery/7.webp" class="gallery__image">
+				</div>
+				<div class="swiper-slide gallery__slide">
+					<img src="img/index-gallery/8.webp" class="gallery__image">
+				</div>
+		    </div>
+
+		    <!-- If we need pagination -->
+		    <div class="swiper-pagination"></div>
+
+			<!-- If we need navigation buttons -->
+			<!-- <div class="swiper-button-prev gallery__button gallery__button-prev"></div> -->
+			<!-- <div class="swiper-button-next gallery__button gallery__button-next"></div> -->
+		</div>	
+
+		<!-- If we need navigation buttons -->
+		<div class="swiper-button-prev gallery__button gallery__button-prev"></div>
+		<div class="swiper-button-next gallery__button gallery__button-next"></div>	
+	</div>
+
+	<script type="text/javascript">
+	
+		var mySwiper = new Swiper('.swiper-container', {			
+			pagination: {
+				el: '.swiper-pagination',
+			},
+
+			slidesPerView: 4,
+			spaceBetween: 16,
+
+			navigation: {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev',
+			}
+		})
+
+	</script>
 
 	<?php require_once "layouts/_footer.php"; ?>
 
