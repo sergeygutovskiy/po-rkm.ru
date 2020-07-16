1221739b77fae707d6ea769321e5cd41ebb7d11d
sergeygutovskiy <sergeygutovsk@gmail.com>
2020-07-16 20:55:13 +0300

    catalog rebuild

--- gates.php
+++ gates.php
@@ -21,7 +21,7 @@
 					<a href="#"><li>Козырьки</li></a>
 					<a href="#"><li>Лестницы</li></a>
 					<a href="#"><li>Декор</li></a>
-					<a href="#"><li>Интерьерный декор</li></a>
+					<a href="#"><li>Интерьер</li></a>
 					
 				</ul>
 			</div>
@@ -30,120 +30,161 @@
 
 		</div>
 		<div class="ndx-header__right-block">
-		<div class="ndx-header__mainbaner">
-		
-		<div class="header">
-			<div class="header__background"></div>
-			<div class="header__border">
-				<div class="ndx-header__textcatalog">
-					<h2>Ворота</h2>	
-					<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum assumenda deleniti quasi, iusto corrupti consequuntur quisquam fugiat necessitatibus magnam explicabo.</p>
-				</div>
-			</div>
-		</div>
-	
-		</div>
-		<div class="ndx-header__goodsblock">
-		
-				
-			<a href="#">
-			<div class="ndx-header__goodscard">
-				<div class="ndx-header__mainphoto">
-				<div class="ndx-header__readmore">
-						<p>Подробнее</p>
-					</div>
-					<img src="img\image26.png">
-					
-				</div>
-				<div class="ndx-header__textaboutgoods">
-					<div class="ndx-header__namegoods">
-						<h3>Название название</h3>
-					</div>
-					<div class="ndx-header__textabout">
-						<p>Название</p>
-					</div>
-					<div class="ndx-header__price">
-						<p>От 1500 ₽</p>
-					</div>
-
-				</div>
-			</div>
-			</a>
-
-			<a href="#">
-			<div class="ndx-header__goodscard">
-				<div class="ndx-header__mainphoto">
-				<div class="ndx-header__readmore">
-						<p>Подробнее</p>
-					</div>
-					<img src="img\image26.png">
-					
-				</div>
-				<div class="ndx-header__textaboutgoods">
-					<div class="ndx-header__namegoods">
-						<h3>Название название</h3>
-					</div>
-					<div class="ndx-header__textabout">
-						<p>Название</p>
-					</div>
-					<div class="ndx-header__price">
-						<p>От 1500 ₽</p>
-					</div>
-
-				</div>
-			</div>
-			</a>
-
-			<a href="#">
-			<div class="ndx-header__goodscard">
-				<div class="ndx-header__mainphoto">
-				<div class="ndx-header__readmore">
-						<p>Подробнее</p>
-					</div>
-					<img src="img\image26.png">
-					
-				</div>
-				<div class="ndx-header__textaboutgoods">
-					<div class="ndx-header__namegoods">
-						<h3>Название название</h3>
-					</div>
-					<div class="ndx-header__textabout">
-						<p>Название</p>
-					</div>
-					<div class="ndx-header__price">
-						<p>От 1500 ₽</p>
-					</div>
-
-				</div>
-			</div>
-			</a>
+			<div class="ndx-header__mainbaner">
 			
-			<a href="#">
-			<div class="ndx-header__goodscard">
-				<div class="ndx-header__mainphoto">
-				<div class="ndx-header__readmore">
-						<p>Подробнее</p>
+			<div class="header">
+				<div class="header__background"></div>
+				<div class="header__border">
+					<div class="ndx-header__textcatalog">
+						<span>Ворота</span>	
+						<br>
+						Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum assumenda deleniti quasi, iusto corrupti consequuntur quisquam fugiat necessitatibus magnam explicabo.
 					</div>
-					<img src="img\image26.png">
-					
 				</div>
-				<div class="ndx-header__textaboutgoods">
-					<div class="ndx-header__namegoods">
-						<h3>Название название</h3>
-					</div>
-					<div class="ndx-header__textabout">
-						<p>Название</p>
-					</div>
-					<div class="ndx-header__price">
-						<p>От 1500 ₽</p>
-					</div>
-
-				</div>
-			</div>
-			</a>
-
 			</div>
 		
+			</div>
+			<div class="ndx-header__goodsblock">
+		
+				<a href="#">
+					<div class="ndx-header__goodscard">
+						<div class="ndx-header__mainphoto">
+						<div class="ndx-header__readmore">
+								<p>Подробнее</p>
+							</div>
+							<img src="img\image26.png">
+							
+						</div>
+						<div class="ndx-header__textaboutgoods">
+							<div class="ndx-header__namegoods">
+								<span>Название название</span>
+							</div>
+							<div class="ndx-header__textabout">
+								Название
+							</div>
+							<div class="ndx-header__price">
+								От 1500 ₽
+							</div>
+						</div>
+					</div>
+				</a>
+
+				<a href="#">
+					<div class="ndx-header__goodscard">
+						<div class="ndx-header__mainphoto">
+						<div class="ndx-header__readmore">
+								<p>Подробнее</p>
+							</div>
+							<img src="img\image26.png">
+							
+						</div>
+						<div class="ndx-header__textaboutgoods">
+							<div class="ndx-header__namegoods">
+								<span>Название название</span>
+							</div>
+							<div class="ndx-header__textabout">
+								Название
+							</div>
+							<div class="ndx-header__price">
+								От 1500 ₽
+							</div>
+						</div>
+					</div>
+				</a>
+
+				<a href="#">
+					<div class="ndx-header__goodscard">
+						<div class="ndx-header__mainphoto">
+						<div class="ndx-header__readmore">
+								<p>Подробнее</p>
+							</div>
+							<img src="img\image26.png">
+							
+						</div>
+						<div class="ndx-header__textaboutgoods">
+							<div class="ndx-header__namegoods">
+								<span>Название название</span>
+							</div>
+							<div class="ndx-header__textabout">
+								Название
+							</div>
+							<div class="ndx-header__price">
+								От 1500 ₽
+							</div>
+						</div>
+					</div>
+				</a>
+
+				<a href="#">
+					<div class="ndx-header__goodscard">
+						<div class="ndx-header__mainphoto">
+						<div class="ndx-header__readmore">
+								<p>Подробнее</p>
+							</div>
+							<img src="img\image26.png">
+							
+						</div>
+						<div class="ndx-header__textaboutgoods">
+							<div class="ndx-header__namegoods">
+								<span>Название название</span>
+							</div>
+							<div class="ndx-header__textabout">
+								Название
+							</div>
+							<div class="ndx-header__price">
+								От 1500 ₽
+							</div>
+						</div>
+					</div>
+				</a>
+
+				<a href="#">
+					<div class="ndx-header__goodscard">
+						<div class="ndx-header__mainphoto">
+						<div class="ndx-header__readmore">
+								<p>Подробнее</p>
+							</div>
+							<img src="img\image26.png">
+							
+						</div>
+						<div class="ndx-header__textaboutgoods">
+							<div class="ndx-header__namegoods">
+								<span>Название название</span>
+							</div>
+							<div class="ndx-header__textabout">
+								Название
+							</div>
+							<div class="ndx-header__price">
+								От 1500 ₽
+							</div>
+						</div>
+					</div>
+				</a>
+
+				<a href="#">
+					<div class="ndx-header__goodscard">
+						<div class="ndx-header__mainphoto">
+						<div class="ndx-header__readmore">
+								<p>Подробнее</p>
+							</div>
+							<img src="img\image26.png">
+							
+						</div>
+						<div class="ndx-header__textaboutgoods">
+							<div class="ndx-header__namegoods">
+								<span>Название название</span>
+							</div>
+							<div class="ndx-header__textabout">
+								Название
+							</div>
+							<div class="ndx-header__price">
+								От 1500 ₽
+							</div>
+						</div>
+					</div>
+				</a>
+
+			</div>
 		</div>
 	</div>
 
