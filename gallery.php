fc4e93551c6daaa3bf6e23008979c32770bd5f75
sergeygutovskiy <sergeygutovsk@gmail.com>
2020-07-17 04:09:11 +0300

    gallery fix

--- gallery.php
+++ gallery.php
@@ -17,32 +17,34 @@
 
 	<?php require_once "layouts/_navigation.php"; ?>
 
-	<div class="gallery">
+	<div class="gallery main-container">
 		<div class="main-title">Галерея</div>
-		<ul class="gallery__categories">
-			<li class="gallery__item">
-				<button class="gallery__category gallery__category--active">Все</button>
-			</li>
-			<li class="gallery__item">
-				<button class="gallery__category">Ворота</button>
-			</li>
-			<li class="gallery__item">
-				<button class="gallery__category">Ограждения</button>
-			</li>
-			<li class="gallery__item">
-				<button class="gallery__category">Козырьки</button>
-			</li>
-			<li class="gallery__item">
-				<button class="gallery__category">Лестницы</button>
-			</li>
-			<li class="gallery__item">
-				<button class="gallery__category">Декор</button>
-			</li>
-			<li class="gallery__item">
-				<button class="gallery__category">Интерьер</button>
-			</li>
-		</ul>
+	</div>
+	<ul class="gallery__categories">
+		<li class="gallery__item">
+			<button class="gallery__category gallery__category--active">Все</button>
+		</li>
+		<li class="gallery__item">
+			<button class="gallery__category">Ворота</button>
+		</li>
+		<li class="gallery__item">
+			<button class="gallery__category">Ограждения</button>
+		</li>
+		<li class="gallery__item">
+			<button class="gallery__category">Козырьки</button>
+		</li>
+		<li class="gallery__item">
+			<button class="gallery__category">Лестницы</button>
+		</li>
+		<li class="gallery__item">
+			<button class="gallery__category">Декор</button>
+		</li>
+		<li class="gallery__item">
+			<button class="gallery__category">Интерьер</button>
+		</li>
+	</ul>
 	
+	<div class="gallery__outside-container">
 		<div class="gallery__container">
 			<div class="gallery__image-block">
 				<img src="img/index-gallery/1.webp" class="gallery__image">
