b674cee622014e1f1753b69ce538c35a1196054e
sergeygutovskiy <sergeygutovsk@gmail.com>
2020-07-19 03:33:09 +0300

    projects page

---
+++ projects.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Проекты</title>
+	<?php require_once "layouts/_header.php"; ?>
+
+	<link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.css">
+	<link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
+
+	<script src="https://unpkg.com/swiper/swiper-bundle.js"></script>
+	<script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+
+	<link rel="stylesheet" type="text/css" href="css/projects.css">
+
+</head>
+<body>
+
+	<?php require_once "layouts/_navigation.php"; ?>
+
+	<div class="main-container projects-container">
+		<div class="main-title">Проекты</div>
+
+		<div class="projects">
+			<div class="projects__item">
+				<div class="projects__image-background"></div>
+				<img src="img/index-gallery/3.webp" class="projects__image">
+				<span class="projects__title">
+					Изготовление ворот, решеток, ограждений
+				</span>
+				<span class="projects__date">6 июня, 2020 г.</span>
+			</div>
+			<div class="projects__item">
+				<div class="projects__image-background"></div>
+				<img src="img/index-gallery/3.webp" class="projects__image">
+				<span class="projects__title">
+					Изготовление ворот
+				</span>
+				<span class="projects__date">6 июня, 2020 г.</span>
+			</div>
+			<div class="projects__item">
+				<div class="projects__image-background"></div>
+				<img src="img/index-gallery/3.webp" class="projects__image">
+				<span class="projects__title">
+					Изготовление ворот, решеток, ограждений
+				</span>
+				<span class="projects__date">6 июня, 2020 г.</span>
+			</div>
+			<div class="projects__item">
+				<div class="projects__image-background"></div>
+				<img src="img/index-gallery/3.webp" class="projects__image">
+				<span class="projects__title">
+					Изготовление ворот, решеток, ограждений
+				</span>
+				<span class="projects__date">6 июня, 2020 г.</span>
+			</div>
+			<div class="projects__item">
+				<div class="projects__image-background"></div>
+				<img src="img/index-gallery/3.webp" class="projects__image">
+				<span class="projects__title">
+					Изготовление ворот, решеток, ограждений
+				</span>
+				<span class="projects__date">6 июня, 2020 г.</span>
+			</div>
+			<div class="projects__item">
+				<div class="projects__image-background"></div>
+				<img src="img/index-gallery/3.webp" class="projects__image">
+				<span class="projects__title">
+					Изготовление ворот, решеток, ограждений
+				</span>
+				<span class="projects__date">6 июня, 2020 г.</span>
+			</div>
+		</div>
+	</div>
+
+	<?php require_once "layouts/_footer.php"; ?>
+
+</body>
+</html>
