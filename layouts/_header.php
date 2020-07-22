be1259903b435ad3b2d97a46d9dafe416c6fa345
sergeygutovskiy <sergeygutovsk@gmail.com>
2020-07-22 13:41:06 +0300

    add favicon

--- layouts/_header.php
+++ layouts/_header.php
@@ -6,6 +6,9 @@
 <link rel="stylesheet" type="text/css" href="css/_navigation.css">
 <link rel="stylesheet" type="text/css" href="css/_footer.css">
 
+<!-- icon -->
+<link rel="shortcut icon" type="image/png" href="img/favicon.png">
+
 <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> -->
 <script src="js/jquery/jquery-3.5.1.min.js" type="text/javascript"></script>
 
