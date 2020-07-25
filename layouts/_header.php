39f4eb776d9e91a487f71b67dc5e186cb848c6a1
sergeygutovskiy <sergeygutovsk@gmail.com>
2020-07-25 17:31:59 +0300

    add focus styles

--- layouts/_header.php
+++ layouts/_header.php
@@ -1,15 +1,16 @@
-<!-- <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;800&display=swap" rel="stylesheet"> -->
+<!-- font awesome -->
 <link href="fonts/font-awesome/css/all.css" rel="stylesheet">
 
 <link rel="stylesheet" type="text/css" href="css/_all.css">
 <link rel="stylesheet" type="text/css" href="css/_main.css">
 <link rel="stylesheet" type="text/css" href="css/_navigation.css">
+<link rel="stylesheet" type="text/css" href="css/_feedback_form.css">
 <link rel="stylesheet" type="text/css" href="css/_footer.css">
 
-<!-- icon -->
+<!-- favicon -->
 <link rel="shortcut icon" type="image/png" href="img/favicon.png">
 
-<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> -->
+<!-- jquery -->
 <script src="js/jquery/jquery-3.5.1.min.js" type="text/javascript"></script>
 
 <meta name="viewport" content="width=device-width, initial-scale=1">
