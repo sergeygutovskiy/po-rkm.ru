487e21eb6d8e5d96c64512cd76b12b5324db8a86
sergeygutovskiy <sergeygutovsk@gmail.com>
2020-07-26 16:56:24 +0300

    full screen image viewer for services pages

--- forging.php
+++ forging.php
@@ -46,127 +46,134 @@
 				Перейти в галерею
 				<i class="fal fa-chevron-right"></i>
 			</a>
-		</section>	
-
-		<section class="gallery-image-viewer">
-			<div class="gallery-image-viewer__background"></div>
-			<div class="gallery-image-viewer__container">
-				<img class="gallery-image-viewer__image" src="img/index-gallery/9.webp">
-				<img class="gallery-image-viewer__image" src="img/index-gallery/3.webp">
-				<img class="gallery-image-viewer__image" src="img/index-gallery/2.webp">
-				<img class="gallery-image-viewer__image" src="img/index-gallery/6.webp">
-			</div>
-
-			<button class="gallery-image-viewer__button gallery-image-viewer__button--left" 
-			onclick="showPrevImage()">
-				<i class="fal fa-chevron-left"></i>
-			</button>
-			<button class="gallery-image-viewer__button gallery-image-viewer__button--right"
-			onclick="showNextImage()">
-				<i class="fal fa-chevron-right"></i>
-			</button>
-			<div class="gallery-image-viewer__counter">1 / 8</div>
 		</section>
-
-		<script>
-			var imagesCount = 4;
-			var elem = null;
-			var elemIndex = 0;
-
-			$(".about-restoration__image-container").on("click", function() {
-
-				elem = $(this).children("img");
-				elemIndex = parseInt(elem.attr("data-key"));
-
-				$(".gallery-image-viewer").addClass("gallery-image-viewer--active");
-				$("body").addClass("main-page-noscroll");
-
-				$(".gallery-image-viewer__button--left").show();
-				$(".gallery-image-viewer__button--right").show();
-
-				if (elemIndex == 0) $(".gallery-image-viewer__button--left").hide();
-				else if (elemIndex == imagesCount - 1) $(".gallery-image-viewer__button--right").hide();
-
-				$(".gallery-image-viewer__counter").text(
-					elemIndex + 1 + " / " + imagesCount
-				);
-
-				var fullScreenImage = $(".gallery-image-viewer__image").eq(elemIndex);
-
-				fullScreenImage
-					.addClass("gallery-image-viewer__image--active");			
-				fullScreenImage
-					.css("left", "calc(50% - " + fullScreenImage.css("width") + " / 2)");
-				fullScreenImage
-					.css("top", "calc(50% - " + fullScreenImage.css("height") + " / 2)");
-			});
-
-			function showPrevImage() {
-				$(".gallery-image-viewer__image").eq(elemIndex)
-					.removeClass("gallery-image-viewer__image--active");	
-
-				$(".gallery-image-viewer__button--right").show();
-
-				elemIndex--;
-				if (elemIndex == 0) $(".gallery-image-viewer__button--left").hide();
-
-				$(".gallery-image-viewer__counter").text(
-					elemIndex + 1 + " / " + imagesCount
-				);
-
-				var fullScreenImage = $(".gallery-image-viewer__image").eq(elemIndex);
-
-				fullScreenImage
-					.addClass("gallery-image-viewer__image--active");			
-				fullScreenImage
-					.css("left", "calc(50% - " + fullScreenImage.css("width") + " / 2)");
-				fullScreenImage
-					.css("top", "calc(50% - " + fullScreenImage.css("height") + " / 2)");
-			}
-
-			function showNextImage() {
-				$(".gallery-image-viewer__image").eq(elemIndex)
-					.removeClass("gallery-image-viewer__image--active");	
-
-				$(".gallery-image-viewer__button--left").show();
-
-				elemIndex++;
-				if (elemIndex == imagesCount - 1) $(".gallery-image-viewer__button--right").hide();
-
-				$(".gallery-image-viewer__counter").text(
-					elemIndex + 1 + " / " + imagesCount
-				);
-
-				var fullScreenImage = $(".gallery-image-viewer__image").eq(elemIndex);
-
-				fullScreenImage
-					.addClass("gallery-image-viewer__image--active");			
-				fullScreenImage
-					.css("left", "calc(50% - " + fullScreenImage.css("width") + " / 2)");
-				fullScreenImage
-					.css("top", "calc(50% - " + fullScreenImage.css("height") + " / 2)");
-			}
-
-			$(".gallery-image-viewer__background").on("click", function() {
-				$(".gallery-image-viewer")
-					.removeClass("gallery-image-viewer--active");
-				$(".gallery-image-viewer__image").eq(elemIndex)
-					.removeClass("gallery-image-viewer__image--active");	
-				$("body").removeClass("main-page-noscroll");
-			});
-
-			window.onresize = function() {
-				var fullScreenImage = $(".gallery-image-viewer__image--active");
-		
-				fullScreenImage
-					.css("left", "calc(50% - " + fullScreenImage.css("width") + " / 2)");
-				fullScreenImage
-					.css("top", "calc(50% - " + fullScreenImage.css("height") + " / 2)");
-			};
-		</script>
 	</main>
 
 	<?php require_once "layouts/_footer.php"; ?>
+
+	<section class="gallery-image-viewer">
+		<div class="gallery-image-viewer__background"></div>
+		<div class="gallery-image-viewer__container">
+			<img class="gallery-image-viewer__image" src="img/index-gallery/9.webp">
+			<img class="gallery-image-viewer__image" src="img/index-gallery/3.webp">
+			<img class="gallery-image-viewer__image" src="img/index-gallery/2.webp">
+			<img class="gallery-image-viewer__image" src="img/index-gallery/6.webp">
+		</div>
+
+		<button class="gallery-image-viewer__button gallery-image-viewer__button--left" 
+		onclick="showPrevImage()">
+			<i class="fal fa-chevron-left"></i>
+		</button>
+		<button class="gallery-image-viewer__button gallery-image-viewer__button--right"
+		onclick="showNextImage()">
+			<i class="fal fa-chevron-right"></i>
+		</button>
+		<div class="gallery-image-viewer__counter">1 / 8</div>
+	</section>
+
+	<script>
+		var imagesCount = 4;
+		var elem = null;
+		var elemIndex = 0;
+
+		var leftButton = $(".gallery-image-viewer__button--left");
+		var rightButton = $(".gallery-image-viewer__button--right");
+
+		var fullScreenImages = $(".gallery-image-viewer__image");
+
+		$(".about-restoration__image-container").on("click", function() {
+
+			// elem = $(this).children("img");
+			elemIndex = parseInt($(this).children("img").attr("data-key"));
+
+			$(".gallery-image-viewer").addClass("gallery-image-viewer--active");
+			$("body").addClass("main-page-noscroll");
+
+			leftButton.show();
+			rightButton.show();
+
+			if (elemIndex == 0) leftButton.hide();
+			else if (elemIndex == imagesCount - 1) rightButton.hide();
+
+			$(".gallery-image-viewer__counter").text(
+				elemIndex + 1 + " / " + imagesCount
+			);
+
+			var fullScreenImage = fullScreenImages.eq(elemIndex);
+
+			fullScreenImage
+				.addClass("gallery-image-viewer__image--active");			
+			fullScreenImage
+				.css("left", "calc(50% - " + fullScreenImage.css("width") + " / 2)");
+			fullScreenImage
+				.css("top", "calc(50% - " + fullScreenImage.css("height") + " / 2)");
+		});
+
+		function showPrevImage() {
+			fullScreenImages.eq(elemIndex)
+				.removeClass("gallery-image-viewer__image--active");	
+
+			rightButton.show();
+
+			elemIndex--;
+			if (elemIndex == 0) leftButton.hide();
+
+			$(".gallery-image-viewer__counter").text(
+				elemIndex + 1 + " / " + imagesCount
+			);
+
+			var fullScreenImage = fullScreenImages.eq(elemIndex);
+
+			fullScreenImage
+				.addClass("gallery-image-viewer__image--active");			
+			fullScreenImage
+				.css("left", "calc(50% - " + fullScreenImage.css("width") + " / 2)");
+			fullScreenImage
+				.css("top", "calc(50% - " + fullScreenImage.css("height") + " / 2)");
+		}
+
+		function showNextImage() {
+			fullScreenImages.eq(elemIndex)
+				.removeClass("gallery-image-viewer__image--active");	
+
+			leftButton.show();
+
+			elemIndex++;
+			if (elemIndex == imagesCount - 1) rightButton.hide();
+
+			$(".gallery-image-viewer__counter").text(
+				elemIndex + 1 + " / " + imagesCount
+			);
+
+			var fullScreenImage = fullScreenImages.eq(elemIndex);
+
+			fullScreenImage
+				.addClass("gallery-image-viewer__image--active");			
+			fullScreenImage
+				.css("left", "calc(50% - " + fullScreenImage.css("width") + " / 2)");
+			fullScreenImage
+				.css("top", "calc(50% - " + fullScreenImage.css("height") + " / 2)");
+		}
+
+		$(".gallery-image-viewer__background").on("click", function() {
+			$(".gallery-image-viewer")
+				.removeClass("gallery-image-viewer--active");
+			
+			$(".gallery-image-viewer__image").eq(elemIndex)
+				.removeClass("gallery-image-viewer__image--active");	
+			
+			$("body").removeClass("main-page-noscroll");
+		});
+
+		window.onresize = function() {
+			var fullScreenImage = $(".gallery-image-viewer__image--active");
+	
+			fullScreenImage
+				.css("left", "calc(50% - " + fullScreenImage.css("width") + " / 2)");
+			fullScreenImage
+				.css("top", "calc(50% - " + fullScreenImage.css("height") + " / 2)");
+		};
+	</script>
 
 </body>
 </html>
--- founding.php
+++ founding.php
@@ -61,5 +61,129 @@
 
 	<?php require_once "layouts/_footer.php"; ?>
 
+	<section class="gallery-image-viewer">
+		<div class="gallery-image-viewer__background"></div>
+		<div class="gallery-image-viewer__container">
+			<img class="gallery-image-viewer__image" src="img/index-gallery/9.webp">
+			<img class="gallery-image-viewer__image" src="img/index-gallery/3.webp">
+			<img class="gallery-image-viewer__image" src="img/index-gallery/2.webp">
+			<img class="gallery-image-viewer__image" src="img/index-gallery/1.webp">
+		</div>
+
+		<button class="gallery-image-viewer__button gallery-image-viewer__button--left" 
+		onclick="showPrevImage()">
+			<i class="fal fa-chevron-left"></i>
+		</button>
+		<button class="gallery-image-viewer__button gallery-image-viewer__button--right"
+		onclick="showNextImage()">
+			<i class="fal fa-chevron-right"></i>
+		</button>
+		<div class="gallery-image-viewer__counter">1 / 8</div>
+	</section>
+
+	<script>
+		var imagesCount = 4;
+		var elem = null;
+		var elemIndex = 0;
+
+		var leftButton = $(".gallery-image-viewer__button--left");
+		var rightButton = $(".gallery-image-viewer__button--right");
+
+		var fullScreenImages = $(".gallery-image-viewer__image");
+
+		$(".about-restoration__image-container").on("click", function() {
+
+			// elem = $(this).children("img");
+			elemIndex = parseInt($(this).children("img").attr("data-key"));
+
+			$(".gallery-image-viewer").addClass("gallery-image-viewer--active");
+			$("body").addClass("main-page-noscroll");
+
+			leftButton.show();
+			rightButton.show();
+
+			if (elemIndex == 0) leftButton.hide();
+			else if (elemIndex == imagesCount - 1) rightButton.hide();
+
+			$(".gallery-image-viewer__counter").text(
+				elemIndex + 1 + " / " + imagesCount
+			);
+
+			var fullScreenImage = fullScreenImages.eq(elemIndex);
+
+			fullScreenImage
+				.addClass("gallery-image-viewer__image--active");			
+			fullScreenImage
+				.css("left", "calc(50% - " + fullScreenImage.css("width") + " / 2)");
+			fullScreenImage
+				.css("top", "calc(50% - " + fullScreenImage.css("height") + " / 2)");
+		});
+
+		function showPrevImage() {
+			fullScreenImages.eq(elemIndex)
+				.removeClass("gallery-image-viewer__image--active");	
+
+			rightButton.show();
+
+			elemIndex--;
+			if (elemIndex == 0) leftButton.hide();
+
+			$(".gallery-image-viewer__counter").text(
+				elemIndex + 1 + " / " + imagesCount
+			);
+
+			var fullScreenImage = fullScreenImages.eq(elemIndex);
+
+			fullScreenImage
+				.addClass("gallery-image-viewer__image--active");			
+			fullScreenImage
+				.css("left", "calc(50% - " + fullScreenImage.css("width") + " / 2)");
+			fullScreenImage
+				.css("top", "calc(50% - " + fullScreenImage.css("height") + " / 2)");
+		}
+
+		function showNextImage() {
+			fullScreenImages.eq(elemIndex)
+				.removeClass("gallery-image-viewer__image--active");	
+
+			leftButton.show();
+
+			elemIndex++;
+			if (elemIndex == imagesCount - 1) rightButton.hide();
+
+			$(".gallery-image-viewer__counter").text(
+				elemIndex + 1 + " / " + imagesCount
+			);
+
+			var fullScreenImage = fullScreenImages.eq(elemIndex);
+
+			fullScreenImage
+				.addClass("gallery-image-viewer__image--active");			
+			fullScreenImage
+				.css("left", "calc(50% - " + fullScreenImage.css("width") + " / 2)");
+			fullScreenImage
+				.css("top", "calc(50% - " + fullScreenImage.css("height") + " / 2)");
+		}
+
+		$(".gallery-image-viewer__background").on("click", function() {
+			$(".gallery-image-viewer")
+				.removeClass("gallery-image-viewer--active");
+			
+			$(".gallery-image-viewer__image").eq(elemIndex)
+				.removeClass("gallery-image-viewer__image--active");	
+			
+			$("body").removeClass("main-page-noscroll");
+		});
+
+		window.onresize = function() {
+			var fullScreenImage = $(".gallery-image-viewer__image--active");
+	
+			fullScreenImage
+				.css("left", "calc(50% - " + fullScreenImage.css("width") + " / 2)");
+			fullScreenImage
+				.css("top", "calc(50% - " + fullScreenImage.css("height") + " / 2)");
+		};
+	</script>
+
 </body>
 </html>
